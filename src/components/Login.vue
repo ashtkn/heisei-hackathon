ce5a4e2b5c659b4c928cadca3440edfd04fd1ca7
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2019-04-21 22:15:29 +0900

    Implement anonymous login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>ログイン画面です</div>
+  <container>
+    <div>ログイン画面です</div>
+    <div>
+      <b-button @click="signInAnonymously">匿名ログイン</b-button>
+    </div>
+  </container>
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
-  name: 'Login'
+  name: 'Login',
+  methods: {
+    signInAnonymously () {
+      firebase.auth().signInAnonymously().then(res => {
+        console.log(res)
+        this.$router.push({
+          path: '/'
+        })
+      }).catch(error => {
+        console.log('SignIn Error: ', error.code, error.message)
+      })
+    }
+  }
 }
 </script>
 
