dba0eba99a0834109d1dbe76c15497179ae1e2cf
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2019-04-21 21:49:38 +0900

    Install Firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+/* Configure Firebase */
+const config = {
+  apiKey: 'AIzaSyCD6Aqza1GTj_7ohAOkKNNOCG7r3k2n_C8',
+  authDomain: 'heisei-hackathon.firebaseapp.com',
+  databaseURL: 'https://heisei-hackathon.firebaseio.com',
+  projectId: 'heisei-hackathon',
+  storageBucket: 'heisei-hackathon.appspot.com',
+  messagingSenderId: '435095458233'
+}
+
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
