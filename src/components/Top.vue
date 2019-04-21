9d27910f21ccccabfcd4e4eb86ae1a83aa88b282
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2019-04-21 22:23:50 +0900

    Implement link to game and post pages

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>トップ画面です</div>
+  <b-container>
+    <div>トップ画面です</div>
+    <div>
+      <b-button v-bind:to="router.game.path">ゲーム開始</b-button>
+    </div>
+    <div>
+      <b-button v-bind:to="router.post.path">新規投稿</b-button>
+    </div>
+  </b-container>
 </template>
 
 <script>
 import firebase from 'firebase'
 export default {
   name: 'Index',
+  data () {
+    return {
+      router: {
+        game: {
+          path: '/game'
+        },
+        post: {
+          path: '/post'
+        }
+      }
+    }
+  },
   created () {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
