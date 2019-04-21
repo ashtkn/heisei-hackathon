8add668cab571b3848bfc33e0e068f96bc0f689b
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2019-04-21 22:14:42 +0900

    Configure Bootstrap

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+/* Configure Bootstrap */
+Vue.use(BootstrapVue)
 
 /* Configure Firebase */
 const config = {
