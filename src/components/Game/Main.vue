c9b7e32d401f314facb95f5b83b43b82c6f7527b
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2019-04-26 13:15:49 +0900

    Add game initialize process

--- src/components/Game/Main.vue
+++ src/components/Game/Main.vue
@@ -7,10 +7,41 @@
 
 <script>
 import Roulette from '@/components/Game/Roulette'
+import firebase from 'firebase'
 export default {
   name: 'Game',
   components: {
     Roulette: Roulette
+  },
+  created () {
+    const db = firebase.firestore()
+    // Create game data
+    const uuid = require('uuid/v1')
+    const gameId = uuid()
+    db.collection('games').doc(gameId).set({
+      gameId: gameId,
+      userId: firebase.auth().currentUser.uid,
+      gameStatus: {
+        currentTurn: 0,
+        currentPlayer: 0,
+        currentStatus: 0
+      },
+      gamePlayers: [
+        {
+          name: 'Player 0',
+          point: 1000
+        },
+        {
+          name: 'Player 1',
+          point: 1000
+        }
+      ]
+    }).then(() => {
+      console.log('Successfully written document')
+    }).catch(error => {
+      console.error(error)
+    })
+    // Fetch spaces data
   }
 }
 </script>
