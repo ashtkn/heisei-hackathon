5a4b0c3adfe6b8c336ff0e3bc02ecd7cc5a260af
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-04-27 12:43:43 +0900

    [update] Space.vue & Map.vue

--- src/components/Game/Map.vue
+++ src/components/Game/Map.vue
@@ -1,16 +1,27 @@
 <template>
+  <div>
   <div>ここにすごろくのマップが入ります</div>
+  <my-space v-bind:spaceRgb="rgbPlus" v-bind:spaceTitle="spaceTitleMap" spaceDate="2003/05/07" spacePoint="+500P"></my-space>
+  <my-space v-bind:spaceRgb="rgbMinus" v-bind:spaceTitle="spaceTitleMap" spaceDate="2003/05/07" spacePoint="-500P"></my-space>
+</div>
 </template>
 
 <script>
 import firebase from 'firebase'
+import Space from '@/components/Game/Space'
 export default {
   name: 'Map',
+  components:{
+    MySpace: Space
+  },
   data () {
     return {
-      spaces: {}
-    }
-  },
+      spaces: {},
+      rgbMinus: 'rgb(255,60,40)',
+      rgbPlus: 'rgb(0,120,255)',
+      spaceTitleMap: '#平成最後のたまごっち'
+        }
+      },
   created () {
     const db = firebase.firestore()
     db.collection('spaces').limit(50).get().then(querySnapshot => {
--- src/components/Game/Space.vue
+++ src/components/Game/Space.vue
@@ -1,10 +1,64 @@
 <template>
-  <div>双六のマスのパーツです</div>
+  <div>
+<svg class="Space"
+ xmlns="http://www.w3.org/2000/svg"
+ xmlns:xlink="http://www.w3.org/1999/xlink"
+ width="200px" height="200px" viewBox="0, 0, 600, 600">
+<defs>
+<filter filterUnits="userSpaceOnUse" id="Filter_0" x="0px" y="0px" width="519px" height="519px"  >
+    <feOffset in="SourceAlpha" dx="0" dy="3" />
+    <feGaussianBlur result="blurOut" stdDeviation="3.162" />
+    <feFlood flood-color="rgb(0, 0, 0)" result="floodOut" />
+    <feComposite operator="atop" in="floodOut" in2="blurOut" />
+    <feComponentTransfer><feFuncA type="linear" slope="0.5"/></feComponentTransfer>
+    <feMerge>
+    <feMergeNode/>
+    <feMergeNode in="SourceGraphic"/>
+  </feMerge>
+</filter>
+<linearGradient id="PSgrad_0" x1="0%" x2="0%" y1="100%" y2="0%">
+  <stop offset="0%" stop-color="rgb(255,255,255)" stop-opacity="0.5" />
+  <stop offset="100%" stop-color="rgb(255,255,255)" stop-opacity="0" />
+</linearGradient>
+
+
+</defs>
+<g filter="url(#Filter_0)">
+<path id="rgb" fill-rule="evenodd"  v-bind:fill="spaceRgb"
+ d="M57.000,6.000 L457.000,6.000 C484.614,6.000 507.000,28.386 507.000,56.000 L507.000,456.000 C507.000,483.614 484.614,506.000 457.000,506.000 L57.000,506.000 C29.386,506.000 7.000,483.614 7.000,456.000 L7.000,56.000 C7.000,28.386 29.386,6.000 57.000,6.000 Z"/>
+</g>
+<path fill="url(#PSgrad_0)"
+ d="M57.000,6.000 L457.000,6.000 C484.614,6.000 507.000,28.386 507.000,56.000 L507.000,456.000 C507.000,483.614 484.614,506.000 457.000,506.000 L57.000,506.000 C29.386,506.000 7.000,483.614 7.000,456.000 L7.000,56.000 C7.000,28.386 29.386,6.000 57.000,6.000 Z"/>
+
+ <!-- <text x="43%" y="20%" text-anchor="middle"
+       font-family="Verdana" font-size="55" fill="white" >
+ {{spaceTitle}}
+ </text> -->
+
+
+ <foreignObject x="25" y="5%" width="450" height="500" font-size="55" >
+ <p xmlns="http://www.w3.org/1999/xhtml" style="  color:white;
+   font-weight: bold; text-shadow: 5px 5px 1px #999999;">{{spaceDate}}</p>
+ </foreignObject>
+
+ <foreignObject x="25" y="33%" width="450" height="500" font-size="55" >
+ <p xmlns="http://www.w3.org/1999/xhtml" style="  color:white;
+   font-weight: bold; text-shadow: 5px 5px 1px #999999;">{{spaceTitle}}</p>
+ </foreignObject>
+
+ <foreignObject x="25" y="60%" width="450" height="500" font-size="80" >
+ <p xmlns="http://www.w3.org/1999/xhtml" style="  color:white;
+   font-weight: bold; text-shadow: 5px 5px 1px #999999;">{{spacePoint}}</p>
+ </foreignObject>
+
+</svg>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Space'
+  name: 'Space',
+  props: ['spaceRgb','spaceDate','spaceTitle','spacePoint']
 }
 </script>
 
