e1efd139419af7503fb4292d71eae6df2f37acd7
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2019-04-29 14:22:46 +0900

    不要なコードを削除

--- src/components/Game/Main.vue
+++ src/components/Game/Main.vue
@@ -1,9 +1,5 @@
 <template>
   <b-container>
-    <div>ゲームのメイン画面です</div>
-    <div>現在の状態: {{currentState}}</div>
-    <div>現在のターン: {{currentTurn}}</div>
-    <div>残りステップ数: {{remainingSteps}}</div>
     <my-side-bar v-bind:game-id="gameId"></my-side-bar>
     <my-roulette v-bind:game-id="gameId"></my-roulette>
     <my-map v-bind:game-id="gameId"></my-map>
@@ -25,10 +21,7 @@ export default {
   },
   data () {
     return {
-      gameId: '',
-      currentTurn: 0,
-      currentState: -1,
-      remainingSteps: 0
+      gameId: ''
     }
   },
   created () {
@@ -52,33 +45,7 @@ export default {
       currentState: 0,
       remainingSteps: 0
     }).then(() => {
-      // Set callback method
-      db.collection('games').doc(this.gameId).onSnapshot(document => {
-        const data = document.data()
-        console.log('Document updated: ', data)
-        const state = data.currentState
-        this.currentTurn = data.currentTurn
-        this.remainingSteps = data.remainingSteps
-        if (state === 0) {
-          // ルーレットを回せる状態
-          this.currentState = 0
-        } else if (state === 1) {
-          // ルーレットを回している状態
-          this.currentState = 1
-        } else if (state === 2) {
-          // ルーレットを回し終わって移動している状態
-          this.currentState = 2
-        } else if (state === 3) {
-          // 移動が終了してマスの詳細を表示している状態
-          this.currentState = 3
-        } else if (state === 4) {
-          // マスの詳細を閉じてポイント計算をしてプレーヤーを変更する状態
-          this.currentState = 4
-        } else {
-          // エラー
-          console.error('State error: ', state)
-        }
-      })
+      console.log('App initialized')
     }).catch(error => {
       console.error(error)
     })
